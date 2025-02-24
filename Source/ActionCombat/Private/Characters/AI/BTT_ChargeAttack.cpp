7cdb7dcb618e1f77b047e7b72a6eb5b8d643b546
Tyler Baylson <thbaylson@gmail.com>
2025-02-23 23:42:58 -0500

    Lesson 6.23: Overriding the TickTask Function.

--- Source/ActionCombat/Private/Characters/AI/BTT_ChargeAttack.cpp
+++ Source/ActionCombat/Private/Characters/AI/BTT_ChargeAttack.cpp
@@ -2,15 +2,38 @@
 #include "AIController.h"
 #include "GameFramework/Character.h"
 #include "Animations/BossAnimInstance.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UBTT_ChargeAttack::UBTT_ChargeAttack()
+{
+	// Note: This must be set in order for the TickTask to ever be called.
+	bNotifyTick = true;
+}
 
 EBTNodeResult::Type UBTT_ChargeAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	UE_LOG(LogTemp, Warning, TEXT("UBTT_ChargeAttack::ExecuteTask"));
 	ControllerRef = OwnerComp.GetAIOwner();
 	CharacterRef = ControllerRef->GetCharacter();
 	BossAnim = Cast<UBossAnimInstance>(CharacterRef->GetMesh()->GetAnimInstance());
 
 	BossAnim->bIsCharging = true;
+	OwnerComp.GetBlackboardComponent()->SetValueAsBool(TEXT("IsReadyToCharge"), false);
 
 	return EBTNodeResult::InProgress;
+}
+
+void UBTT_ChargeAttack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	bool bIsReadyToCharge{ OwnerComp.GetBlackboardComponent()->GetValueAsBool(TEXT("IsReadyToCharge")) };
+
+	if (bIsReadyToCharge)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(TEXT("IsReadyToCharge"), false);
+		ChargeAtPlayer();
+	}
+}
+
+void UBTT_ChargeAttack::ChargeAtPlayer()
+{
+	UE_LOG(LogTemp, Warning, TEXT("UBTT_ChargeAttack::ChargeAtPlayer"));
 }
--- Source/ActionCombat/Public/Characters/AI/BTT_ChargeAttack.h
+++ Source/ActionCombat/Public/Characters/AI/BTT_ChargeAttack.h
@@ -19,7 +19,14 @@ class ACTIONCOMBAT_API UBTT_ChargeAttack : public UBTTaskNode
 	ACharacter* CharacterRef;
 
 	class UBossAnimInstance* BossAnim;
+
+protected:
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 	
 public:
+	UBTT_ChargeAttack();
+
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	void ChargeAtPlayer();
 };
